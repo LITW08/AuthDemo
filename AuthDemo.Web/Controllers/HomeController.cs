7d4b6f819182362d5f000660ff8386b4d4cc85df
Avrumi Friedman <a.friedman07@gmail.com>
2021-03-17 21:18:40 -0400

    Added homework instructions

--- AuthDemo.Web/Controllers/HomeController.cs
+++ AuthDemo.Web/Controllers/HomeController.cs
@@ -40,4 +40,25 @@ namespace AuthDemo.Web.Controllers
         }
 
     }
+
+    /*Create an application where users can post simple ads for others to see.
+     An ad consists of a Title, Phone number and description. To create an ad, a user must be logged in. 
+    
+    Here are the basic pages you'll need:
+    
+    Home Page - on this page, display all the ads in the system. If the current user is logged in,
+    and some of the ads belong to them, those ads should also have a delete button that gives that user 
+    the ability to delete that ad.
+    
+    New Ad - (Put a link to this page up in the nav bar using the layout page) - When this page is accessed, 
+    if the current user isn't logged in, they should be redirected to a login page. 
+    If they are logged in however, they should be shown a form where they can create a new ad. 
+    
+    My Account (this should be shown in the nav bar using the layout as well - however, 
+    this link should only be shown if the current user is logged in) - 
+    on this page, display all the ads that the current user has created as well as a delete button for each one.
+    
+    Finally, in the nav bar on top, if the current user is logged in, display a Logout link. 
+    If they're NOT logged in, display a link called Login.
+    */
 }
